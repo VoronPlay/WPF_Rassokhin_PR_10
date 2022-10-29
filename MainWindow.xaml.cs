f956efac2608aeba081116c559c8e2fcd4cefb0c
u31510 <u31510@P31510>
2022-10-29 10:05:51 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Rassokhin_PR_10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1= new Window1();
+            window1.Show();
+        }
+
+        private void Next_Click_2(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void Delet_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Remove(textBox1.Text);
+        }
+
+        private void Soft_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Content", System.ComponentModel.ListSortDirection.Ascending));
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Rassokhin_PR_10
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Delet_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Remove(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void Soft_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Content", System.ComponentModel.ListSortDirection.Ascending));
+        }
+
+        private void CoffieCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            cofe_1.Source = new BitmapImage(new Uri("Image/cofe.jpg", UriKind.Relative));
+        }
+
+        private void TeaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            cofe_1.Source = new BitmapImage(new Uri("Image/Tea.jpg", UriKind.Relative));
+        }
+
+        private void OrangeJuiceCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            cofe_1.Source = new BitmapImage(new Uri("Image/Oranje.jpg", UriKind.Relative));
+        }
+
+        private void MilkCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            cofe_1.Source = new BitmapImage(new Uri("Image/Milk.jpg", UriKind.Relative));
+        }
+
+        private void IcedTeaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            cofe_1.Source = new BitmapImage(new Uri("Image/Ice_Tea.jpg", UriKind.Relative));
+        }
+
+        private void MangoShakeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            cofe_1.Source = new BitmapImage(new Uri("Image/Mango.jpg", UriKind.Relative));
+        }
+    }
+}
